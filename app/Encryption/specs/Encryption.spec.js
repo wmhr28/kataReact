f139ae47be4cbcc0091b9819cbae79708f7cdd4e
Mauricio Herrera <15660754+wmhr28@users.noreply.github.com>
2017-10-30 00:10:59 -0500

    Encontré un error en la kata, para que esté bien debe tener la siguiente entrada 20,XY 9,CNAVRWJA 5,QF 18,CSLS y devuelve DE TERMINAR LA KATA

--- app/Encryption/specs/Encryption.spec.js
+++ app/Encryption/specs/Encryption.spec.js
@@ -13,23 +13,33 @@ describe('Encryption', () => {
     instance = component.instance();
   });
   describe('decrypt', () => {
-    fit('entrada POHAOB devuelve BATMAN', () => {
+    it('entrada POHAOB devuelve BATMAN', () => {
       let resp = instance.decrypt(14,'POHAOB');
       expect(resp).toEqual('BATMAN');
     });
-    fit('entrada VCZO devuelve HOLA', () => {
+    it('entrada VCZO devuelve HOLA', () => {
       let resp = instance.decrypt(14,'VCZO');
       expect(resp).toEqual('HOLA');
     });
-    fit('entrada PCAPO devuelve HOLA', () => {
+    it('entrada PCAPO devuelve HOLA', () => {
       let resp = instance.decrypt(14,'PCAPO');
       expect(resp).toEqual('BOMBA');
     });
-    fit('entrada 1,FTUBT 9,LNALJ devuelve ESTAS CERCA', () => {
+    it('entrada 1,FTUBT 9,LNALJ devuelve ESTAS CERCA', () => {
       let resp = instance.decrypt(1,'FTUBT');
       expect(resp).toEqual('ESTAS');
       resp = instance.decrypt(9,'LNALJ');
       expect(resp).toEqual('CERCA');
+    });
+    it('entrada 20,XY 9,CNAVRWJA 5,QF 18,CSLS devuelve DE TERMINAR LA KATA', () => {
+      let resp = instance.decrypt(20,'XY');
+      expect(resp).toEqual('DE');
+      resp = instance.decrypt(9,'CNAVRWJA');
+      expect(resp).toEqual('TERMINAR');
+      resp = instance.decrypt(5,'QF');
+      expect(resp).toEqual('LA');
+      resp = instance.decrypt(18,'CSLS');
+      expect(resp).toEqual('KATA');
     });
   });
 });
