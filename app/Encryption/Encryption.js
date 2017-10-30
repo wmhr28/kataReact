a21f83e56e2807dc50b97033f5cb6bb2726187ee
Mauricio Herrera <15660754+wmhr28@users.noreply.github.com>
2017-10-30 01:36:03 -0500

    Permite colocar la linea y el texto encriptado para probar toda las kata en el navegador

--- app/Encryption/Encryption.js
+++ app/Encryption/Encryption.js
@@ -4,27 +4,38 @@ import Alphabet from '../Alphabet/Alphabet';
 class Encryption extends Component {
   constructor(props) {
     super(props);
-    this.state = { resp: '' };
-    this.handler = this.handler.bind(this);
+    this.state = { resp: '', inputLine: 0, inputText: '' };
+    this.handlerLine = this.handlerLine.bind(this);
+    this.handlerText = this.handlerText.bind(this);
   }
-  decrypt(indexLine, text) { 
+  decrypt(indexLine, text) {
     let objAlph = new Alphabet();
     return [].map.call(text, (char) => {
       let position = objAlph.getPositionAlph(char, indexLine);
       return objAlph.getLetraAlphCorrecto(position);
-    }).join(''); 
+    }).join('');
   }
-  handler(e) {
-    let val = e.target.value.toUpperCase();
-    e.target.value = val;
-    this.setState({ resp: this.decrypt(14, val) });
+  calculate(inputLine, inputText) {
+    this.setState({ resp: this.decrypt(inputLine, inputText) });
+  }
+  handlerLine(e) {
+   let _inputLine=e.target.value;
+    this.setState({ inputLine: _inputLine });
+    this.calculate(_inputLine,this.state.inputText);
+  }
+  handlerText(e) {
+    let _inputText=e.target.value.toUpperCase();
+    this.setState({ inputText: _inputText });
+    this.calculate(this.state.inputLine,_inputText);
   }
   render() {
     return (
       <div className="container">
         <div>
-          Encrypted text (14): <input type="text"
-            onChange={this.handler} />
+          Line: <input type="text"
+            onChange={this.handlerLine} />
+          Encrypted text: <input type="text"
+            onChange={this.handlerText} />
           <h4>Answer: {this.state.resp}</h4>
         </div>
 
